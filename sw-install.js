ff803bed5e339faff0d2b29a339d4704db97c166
markjasonanderson <markjasonanderson@gmail.com>
2017-09-27 18:33:52 +1000

    install command

---
+++ sw-install.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+var gtapi = require('./gt-api.js');
+program
+  .arguments('<slot> <urn> <server>')
+  .option('-v, --verbose', 'Be verbose')
+  .option('-j, --json', 'Print repsonse as JSON')
+  .action(function(slot, urn, server) {
+    if (program.verbose) gtapi.log_level = 1;
+    print_json = program.json;
+    console.log('installing application %s on device %s via %s', slot , urn, server);
+
+    gtapi.software_install(slot, urn, server, function(response)
+    {
+      if (print_json) console.log(JSON.stringify(response, null, 2));
+      else console.log(response.code);
+    });
+  })
+  .parse(process.argv);
