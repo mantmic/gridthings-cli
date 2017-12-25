871cd72daf2de50729de1ef718f0bdc462ebce7c
ziedmahdi <ziedmahdi@gmail.com>
2017-12-25 14:32:44 +0000

    add dred commands

---
+++ dred-context.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var gtapi = require('./gt-api.js');
+var Lazy = require('lazy.js');
+var Helpers = require('./dred-helpers.js');
+
+
+var wrongArguments = true;
+
+program
+  .arguments('<context> <dredClass> <utcOffset> <urn> [server]')
+  .option('-v, --verbose', 'Be verbose')
+  .action(function(context, dredClass, utcOffset, urn, server) {
+    wrongArguments = false;
+    var values = Helpers.convertArgumentsToUsedValues({context, dredClass, utcOffset, urn, server});
+
+    Helpers.checkDredArguments(values);
+
+    if (program.verbose) {
+      gtapi.log_level = 1;
+    }
+
+    //construct the body of the request
+    var payload = {
+      id: 0,
+      resources: Helpers.buildPayloadResources(Lazy(values).pick(['context', 'dredClass', 'utcOffset']).toObject())
+    };
+
+    gtapi.core_put('33000/' + 0, payload, urn, server, function () {
+      console.info('Configuration was applied');
+    }, function (error) {
+      Helpers.displayError(error);
+    });
+  })
+  .parse(process.argv);
+
+if (wrongArguments == true) {
+  program.help();
+}
---
+++ dred-stop.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var gtapi = require('./gt-api.js');
+var Helpers = require('./dred-helpers.js');
+
+var wrongArguments = true;
+
+program
+  .arguments('<instance> <urn> [server]')
+  .option('-v, --verbose', 'Be verbose')
+  .action(function(instance, urn, server) {
+    wrongArguments = false;
+    var values = Helpers.convertArgumentsToUsedValues({instance, urn, server});
+
+    Helpers.checkDredArguments(values);
+
+    if (program.verbose) {
+      gtapi.log_level = 1;
+    }
+
+    gtapi.core_exec('33001/' + instance + '/10', null, urn, server, function () {
+      console.info('Configuration was applied');
+    }, function (error) {
+      Helpers.displayError(error);
+    });
+  })
+  .parse(process.argv);
+
+if (wrongArguments == true) {
+  program.help();
+}
