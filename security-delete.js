66b15276a842779dd97876f18ed6a4cff7062888
markjasonanderson <markjasonanderson@gmail.com>
2018-01-05 07:12:50 +1100

    add security command files

---
+++ security-delete.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+var gtapi = require('./gt-api.js');
+
+program
+  .arguments('<urn> [server]')
+  .option('-v, --verbose', 'Be verbose')
+  .action(function(urn, server) {
+    gtapi.log_level = program.verbose ? 1 : 0;
+    print_json = false;
+    gtapi.security_delete_endpoint(urn, server, function(response)
+    {
+      if (print_json)
+      {
+        console.log(JSON.stringify(response, null, 2));
+      }
+      else
+      {
+        console.log(response.status + " " + response.text);
+      }
+    }, 
+    function(err)
+    {
+      console.log("" + err.response.text);
+    })
+  })
+  .parse(process.argv);
